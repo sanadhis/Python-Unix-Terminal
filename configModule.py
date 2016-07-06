6f130cfbe03ba4e04d41914668fcfb539992789e
sanadhis <sanadhi.sutandi@ui.ac.id>
2016-07-06 18:26:55 +0200

    create config

--- configModule.py
+++ configModule.py
@@ -8,3 +8,13 @@ def getOraclePath():
 
 def sendOsCommand(command):
     os.system(command)    
+
+def getDomainPath(domainName):
+    baseOraclePath = getOraclePath()
+    domainPath = baseOraclePath + '/user_projects/domains/' + domainName
+    return domainPath
+
+def sendWLSTCommand(filename):
+    baseOraclePath = getOraclePath()
+    wlstPath = baseOraclePath + '/oracle_common/common/bin/wlst.sh '
+    sendOsCommand(wlstPath + filename)
---
+++ createInstance.py
@@ -0,0 +1,31 @@
+from configModule import getDomainPath,sendWLSTCommand
+
+domainName = 'base_domain'
+configurationName = 'defaultConfig'
+machineName = 'machine0'
+
+def setConfiguration():
+    global domainName, configurationName, machineName
+    domainName = raw_input('Enter domain name: ')
+    configurationName = raw_input('Enter Configuration name: ')
+    listenerPort = raw_input("Enter OTD's instances port: ")
+    serverName = raw_input("Enter OTD's server name: ")
+    originServer = raw_input("Enter the back-end's Server: ")
+
+def createConfiguration():
+    domainPath = getDomainPath(domainName)
+    file = open('wlstScript.py','w')
+    file.write("readDomain('"+domainPath+"')\n")
+    file.write("props={}\n")
+    file.write("props['configuration'] = '"+configurationName+"'\n")
+    file.write("props['listener-port'] = '"+listenerPort+"'\n")
+    file.write("props['server-name'] = '"+serverName+"'\n")
+    file.write("props['origin-server'] = '"+originServer+"'\n")
+    file.write("otd_createConfiguration(props)\n")
+    file.write("updateDomain()\n")
+    file.write("closeDomain()\n")
+    file.close()
+    sendWLSTCommand('wlstScript.py')
+
+setConfiguration()
+createConfiguration()
